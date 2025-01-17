07f72af3722af83d8ff58036cf7ff57a6246b013
ElMino94 <trestoux@gaming.tech>
2025-01-17 10:48:43 +0100

    class

--- projet de jeu d'aventure en 2D/main.cpp
+++ projet de jeu d'aventure en 2D/main.cpp
@@ -1,6 +1,75 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include <iostream>
+using namespace sf;
 using namespace std;
 
-int main() {
+class Entity {
+public:
 
+    virtual void update(float deltaTime) = 0;
+    virtual void draw(sf::RenderWindow & window) = 0;
+
+};
+
+class Player : public Entity {
+private:
+    CircleShape shape;
+    Vector2f position;
+    Vector2f velocity;
+public:
+    
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+    RenderWindow window(VideoMode(1920, 1080), "Jeu d'aventure");
+
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
+
+            if (event.type == Event::KeyPressed) {
+                if (event.key.code == Keyboard::A)
+                    cout << "La touche A a été pressée!" << endl;
+                if (event.key.code == Keyboard::Escape)
+                    window.close(); // Fermer avec Échap
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
 }
