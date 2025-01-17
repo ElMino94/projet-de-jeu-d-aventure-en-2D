9cca480ffb158103b82b31a0363c55fb03983a36
ElMino94 <trestoux@gaming.tech>
2025-01-17 11:39:27 +0100

    mise en place du player
    ajout des mouvement = affichage d'un player

--- projet de jeu d'aventure en 2D/main.cpp
+++ projet de jeu d'aventure en 2D/main.cpp
@@ -7,51 +7,64 @@ using namespace std;
 class Entity {
 public:
 
-    virtual void update(float deltaTime) = 0;
-    virtual void draw(sf::RenderWindow & window) = 0;
+    virtual void maj(float deltaTime) = 0;
+    virtual void draw(RenderWindow & window) = 0;
 
 };
 
 class Player : public Entity {
 private:
-    CircleShape shape;
+
+    CircleShape player;
     Vector2f position;
     Vector2f velocity;
+    float speed;
+
 public:
-    
+
+    Player() : position(100.f, 100.f), velocity(0.f, 0.f), speed(200) {
+        player.setRadius(50.f);
+        player.setFillColor(Color::White);
+        player.setPosition(position);
+    }
+
+    void touche() {
+
+        velocity = Vector2f(0.f, 0.f);
+
+        if (Keyboard::isKeyPressed(Keyboard::Z)) {
+            velocity.y -= speed;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::S)) {
+            velocity.y += speed;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Q)) {
+            velocity.x -= speed;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::D)) {
+            velocity.x += speed;
+        }
+    }
+
+    void maj(float deltaTime) override {
+        position += velocity * deltaTime;
+        player.setPosition(position);
+    }
+
+    void draw(RenderWindow& window) override {
+        window.draw(player);
+    }
 
 };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 int main() {
+
     RenderWindow window(VideoMode(1920, 1080), "Jeu d'aventure");
+    window.setFramerateLimit(60);
+
+    Player player;
+    Clock clock;
 
     while (window.isOpen()) {
         Event event;
@@ -67,7 +80,14 @@ int main() {
             }
         }
 
+        player.touche();
+        float deltaTime = clock.restart().asSeconds();
+
+        player.maj(deltaTime);
+
+
         window.clear();
+        player.draw(window);
         window.display();
     }
 
